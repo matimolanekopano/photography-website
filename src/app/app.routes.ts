3f56ba1dd3832b5431d106f2b9c56c2f6c474141
Kopano Matimolane <128792132+kopanomatimolane@users.noreply.github.com>
2025-01-14 15:48:15 +0200

    Code from 14-01
    Add the navbar and kinda designed the other pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { LoveComponent } from './love/love.component';
 import { MdComponent } from './md/md.component';
 import { PictureDisplayComponent } from './picture-display/picture-display.component';
 import { ContactComponent } from './contact/contact.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 export const routes: Routes = [
-    { path: '', component: HomepageComponent },
+    { path: 'home', component: HomepageComponent },
     { path: 'birthdays', component: BirthdaysComponent },
     { path: 'love', component: LoveComponent },
     { path: 'md', component: MdComponent },
     { path: 'picture', component: PictureDisplayComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'pricing', component: PricingComponent }
 ];
